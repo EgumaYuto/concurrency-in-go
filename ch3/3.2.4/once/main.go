835eceea13155f4b816b0e4b86eab5934c4a2018
yuto.eguma <yuto.eguma@gmail.com>
2021-09-24 16:18:50 +0900

    3.2.4 once

---
+++ ch3/3.2.4/once/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "sync"
+
+func main() {
+	// var count int
+	// increment := func() {
+	// 	count++
+	// }
+
+	// var once sync.Once
+
+	// var increments sync.WaitGroup
+	// increments.Add(100)
+	// for i := 0; i < 100; i++ {
+	// 	go func() {
+	// 		defer increments.Done()
+	// 		once.Do(increment)
+	// 	}()
+	// }
+
+	// increments.Wait()
+	// fmt.Printf("count is %d\n", count)
+
+	//////////////////////////////////////
+
+	// var count int
+	// increment := func() { count++ }
+	// decrement := func() { count-- }
+
+	// var once sync.Once
+	// once.Do(increment)
+	// once.Do(decrement)
+	// fmt.Printf("Count: %d\n", count)
+
+	/////////////////////////////////////
+
+	var onceA, onceB sync.Once
+	var initB func()
+	initA := func() { onceB.Do(initB) }
+	initB = func() { onceB.Do(initA) }
+	onceA.Do(initA)
+}
